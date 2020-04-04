ec5294895ffeb260105e24ff514cfa0831bb8989
mac5562 <55428222+mac5562@users.noreply.github.com>
2020-04-04 11:54:37 +0200

    Simplex(Console) Feltöltése
    Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

---
+++ Simplex(Console)/Simplex(Console)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplex_Console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LPP feladat;
+            ObjectiveFunction of;
+            Constraint cs1;
+            Constraint cs2;
+            Constraint cs3;
+           
+
+
+
+            double[] tomb1 = { 5, 4, 3 };
+
+            double[] tombcs1 = {2,3,1};
+            double[] tombcs2 = {4,1,2};
+            double[] tombcs3 = {3,4,2};
+
+            cs1 = new Constraint(tombcs1,5);
+            cs2 = new Constraint(tombcs2,11);
+            cs3 = new Constraint(tombcs3,8);
+
+            of = new ObjectiveFunction(tomb1);
+            Constraint[] css = { cs1, cs2, cs3 };
+
+            feladat = new LPP(of,css);
+
+            feladat.Solve();
+
+            Console.ReadLine();
+
+        }
+    }
+}
